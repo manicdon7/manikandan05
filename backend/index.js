28c36376158d852adecacd2e987c34e3bb82073d
manicdon7 <manicdon7@gmail.com>
2024-03-17 14:04:38 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,7 @@ const bcrypt = require('bcrypt');
 const app = express();
 const port = 5000;
 
-app.use(cors({
-  origin: ["https://manikandan05.vercel.app/"],
-  methods: ["POST", "GET"],
-  credentials: true
-}));
+app.use(cors({origin:'https://manikandan05.vercel.app'}));
 app.use(bodyParser.json());
 const dburi = process.env.dbURI;
 mongoose.connect(dburi, {
