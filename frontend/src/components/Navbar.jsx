34cf62278bd84d696c8af26ce611ac4df8c0f673
manicdon7 <manicdon7@gmail.com>
2024-03-17 15:22:03 +0530

    resume

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const DesktopNavbar = () => {
         <a href='/certificates'>Certificate</a>
       </div>
       <div className='py-5 px-20 text-white'>
-        <a href='src/assets/ManikandanResume.pdf' type='application/pdf' download='Manikandan Resume' className='border-2 border-glow transform text-xl rounded-3xl px-3 py-2 transform scale-100 transition-transform duration-300 ease-in-out hover:scale-105' style={{ borderColor: '#FF4900' }}>Download Resume</a>
+        <a href='src/assets/ManikandanResume.pdf' type='pdf' download='Manikandan Resume' className='border-2 border-glow transform text-xl rounded-3xl px-3 py-2 transform scale-100 transition-transform duration-300 ease-in-out hover:scale-105' style={{ borderColor: '#FF4900' }}>Download Resume</a>
       </div>
     </nav>
   );
